560343f1272f1a648ac63d421e8e26a7a1c739f1
ehom <erwin.hom@puppet.com>
2018-11-25 14:12:35 -0800

    decouple the string decoration-type methods from the class

--- lib/fake-loc-lib.rb
+++ lib/fake-loc-lib.rb
@@ -1,12 +1,48 @@
-class FakeLoc
+
+class AccentService
+  def initialize
+    # TODO need to provide a character map
+  end
+
+  def localize(a)
+    # TODO not implemented yet
+    a
+  end
+end
+
+class BracketService
   attr_accessor :left_bracket, :right_bracket
+
+  def initialize
+    @left_bracket  = "\u00ab"
+    @right_bracket = "\u00bb"
+  end
+
+  def localize(a)
+    a.insert(0, @left_bracket).push(@right_bracket)
+  end
+end
+
+class ExpanderService
   attr_accessor :padding_char, :expansion
 
   def initialize
     @padding_char = "_"
-    @expansion = 0.30
-    @left_bracket = "\u00ab"
-    @right_bracket = "\u00bb"
+    @expansion    = 0.30
+  end
+
+  def localize(a)
+    padding = [].fill(@padding_char, 0, (a.length * expansion).ceil).join('')
+    a.insert(0, padding).push(padding)
+  end
+end
+
+
+class FakeLoc
+  def initialize
+    @accenter  = AccentService.new
+    @bracketer = BracketService.new
+    @expander  = ExpanderService.new
   end
 
   def localize(s)
@@ -15,18 +51,17 @@ class FakeLoc
   end
 
   def accent
-    # TODO
+    @a = @accenter.localize(@a)
     self
   end
 
   def expand
-    padding = [].fill(@padding_char, 0, (@a.length * @expansion).ceil).join('')
-    @a.insert(0, padding).push(padding)
+    @a = @expander.localize(@a)
     self
   end
 
   def bracket
-    @a.insert(0, @left_bracket).push(@right_bracket)
+    @a = @bracketer.localize(@a)
     self
   end
 
